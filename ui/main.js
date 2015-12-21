123061d80f6f8bf58df3b87b6f7b6d11462df28f
Dan Jacob <danjac354@gmail.com>
2015-12-21 20:19:02 +0200

    Stub react setup

---
+++ ui/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Container = props => {
+  return <div>Open for business!!!</div>;
+}
+
+ReactDOM.render(<Container />, document.getElementById("app"));
