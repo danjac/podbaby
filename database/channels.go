b1298f9f0127da6ca4a7c4ad6e8d789bd15aef00
Dan Jacob <danjac354@gmail.com>
2015-12-27 15:38:28 +0200

    Channel query

--- database/channels.go
+++ database/channels.go
@@ -7,11 +7,20 @@ import (
 
 // ChannelDB database model
 type ChannelDB interface {
+	GetByID(int64, int64) (*models.Channel, error)
 	Create(*models.Channel) error
 }
 
 type defaultChannelDBImpl struct {
 	*sqlx.DB
+}
+
+func (db *defaultChannelDBImpl) GetByID(id int64, userID int64) (*models.Channel, error) {
+	sql := `SELECT channels.*,
+        EXISTS(SELECT id FROM subscriptions WHERE channel_id=channels.id AND user_id=$1) AS is_subscribed
+        WHERE id=$2`
+	channel := &models.Channel{}
+	return channel, db.Get(channel, sql, userID, id)
 }
 
 func (db *defaultChannelDBImpl) Create(ch *models.Channel) error {
--- models/channel.go
+++ models/channel.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type Channel struct {
-	ID          int64     `db:"id" json:"id"`
-	Title       string    `db:"title" json:"title"`
-	Description string    `db:"description" json:"description"`
-	Image       string    `db:"image" json:"image"`
-	URL         string    `db:"url" json:"url"`
-	CreatedAt   time.Time `db:"created_at" json:"createdAt"`
+	ID           int64     `db:"id" json:"id"`
+	Title        string    `db:"title" json:"title"`
+	Description  string    `db:"description" json:"description"`
+	Image        string    `db:"image" json:"image"`
+	URL          string    `db:"url" json:"url"`
+	IsSubscribed bool      `db:"is_subscribed" json:"isSubscribed"`
+	CreatedAt    time.Time `db:"created_at" json:"createdAt"`
 }
