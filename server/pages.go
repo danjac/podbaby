4f20598fd118379ae9af554bbff20a99257456d2
Dan Jacob <danjac354@gmail.com>
2016-01-02 03:20:50 +0200

    Use correct dev static url

--- server/pages.go
+++ server/pages.go
@@ -7,10 +7,17 @@ import (
 )
 
 func (s *Server) indexPage(w http.ResponseWriter, r *http.Request) {
+
+	var dynamicContentURL string
+	if s.Config.Env == "dev" {
+		dynamicContentURL = s.Config.DynamicContentURL
+	} else {
+		dynamicContentURL = s.Config.StaticURL
+	}
 	user, _ := s.getUserFromCookie(r)
 	csrfToken := nosurf.Token(r)
 	ctx := map[string]interface{}{
-		"dynamicContentURL": s.Config.DynamicContentURL,
+		"dynamicContentURL": dynamicContentURL,
 		"staticURL":         s.Config.StaticURL,
 		"csrfToken":         csrfToken,
 		"user":              user,
