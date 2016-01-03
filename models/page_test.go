55fee4051ad25c44006ea97169815ae826a75f5d
Dan Jacob <danjac354@gmail.com>
2016-01-03 11:26:08 +0200

    Test for pages

---
+++ models/page_test.go
@@ -0,0 +1,33 @@
+package models
+
+import "testing"
+
+func TestPageIfEmpty(t *testing.T) {
+	p := NewPage(1, 0)
+	if p.NumPages != 0 {
+		t.Fail()
+	}
+	if p.Offset != 0 {
+		t.Fail()
+	}
+}
+
+func TestPageIfNotEmpty(t *testing.T) {
+	p := NewPage(1, 100)
+	if p.NumPages != 10 {
+		t.Fail()
+	}
+	if p.Offset != 0 {
+		t.Fail()
+	}
+}
+
+func TestPageOffset(t *testing.T) {
+	p := NewPage(5, 100)
+	if p.NumPages != 10 {
+		t.Fail()
+	}
+	if p.Offset != 40 {
+		t.Fail()
+	}
+}
