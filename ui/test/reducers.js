b054e4c67dfe721e854b524618d0024112faa694
Dan Jacob <danjac354@gmail.com>
2016-01-12 01:42:00 +0200

    Player tests

--- ui/test/reducers.js
+++ ui/test/reducers.js
@@ -36,24 +36,181 @@ describe("Player", function () {
   });
 
   it('Bookmarks the podcast', function() {
+
+    const podcast = {
+      id: 100,
+      title: "A podcast"
+    };
+
+    const state = Object.assign({}, initialState, { podcast: podcast });
+
+    const action = {
+      type: Actions.ADD_BOOKMARK,
+      payload: 100
+    };
+
+    const newState = playerReducer(state, action);
+
+    assert.ok(newState.podcast.isBookmarked)
+
   });
 
-  it('Bookmarks the podcast if player empty', function() {
+  it('Does not bookmark the podcast if not the player podcast', function() {
+
+    const podcast = {
+      id: 100,
+      title: "A podcast"
+    };
+
+    const state = Object.assign({}, initialState, { podcast: podcast });
+
+    const action = {
+      type: Actions.ADD_BOOKMARK,
+      payload: 200
+    };
+
+    const newState = playerReducer(state, action);
+
+    assert.notOk(newState.podcast.isBookmarked)
+
+  });
+
+  it('Does not bookmark the podcast if player empty', function() {
+
+    const action = {
+      type: Actions.ADD_BOOKMARK,
+      payload: 200
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.podcast, null)
+
   });
 
   it('Removes the bookmark from the podcast', function() {
+
+    const podcast = {
+      id: 100,
+      title: "A podcast",
+      isBookmarked: true
+
+    };
+
+    const state = Object.assign({}, initialState, { podcast: podcast });
+
+    const action = {
+      type: Actions.DELETE_BOOKMARK,
+      payload: 100
+    };
+
+    const newState = playerReducer(state, action);
+
+    assert.notOk(newState.podcast.isBookmarked)
+
+  });
+
+  it('Does not remove the bookmark from the podcast if not player', function() {
+    const podcast = {
+      id: 100,
+      title: "A podcast",
+      isBookmarked: true
+    };
+
+    const state = Object.assign({}, initialState, { podcast: podcast });
+
+    const action = {
+      type: Actions.DELETE_BOOKMARK,
+      payload: 200
+    };
+
+    const newState = playerReducer(state, action);
+
+    assert.ok(newState.podcast.isBookmarked)
+
+  });
+
+  it('Does not remove the bookmark from the podcast if player empty', function() {
+    const action = {
+      type: Actions.DELETE_BOOKMARK,
+      payload: 200
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.podcast, null)
+
   });
 
   it('Updates the current play time', function() {
+    const action = {
+      type: Actions.PLAYER_TIME_UPDATE,
+      payload: 30
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.currentTime, 30)
+
   });
 
   it('Reloads the player if none currently playing', function() {
+
+    const action = {
+      type: Actions.RELOAD_PLAYER,
+      payload: null
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.podcast, null);
+    assert.equal(newState.currentTime, 0);
+
   });
 
   it('Reloads the player if podcast currently playing', function() {
+
+    const podcast = {
+      id: 100,
+      title: "A podcast"
+    };
+
+    const action = {
+      type: Actions.RELOAD_PLAYER,
+      payload: {
+        podcast,
+        currentTime: 100,
+        isPlaying: true
+      }
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.podcast.id, podcast.id);
+    assert.equal(newState.currentTime, 100);
+
   });
 
   it('Closes the player', function() {
+    const podcast = {
+      id: 100,
+      title: "A podcast"
+    };
+
+    const state = Object.assign({}, initialState, {
+      podcast: podcast
+    });
+
+    const action = {
+      type: Actions.CLOSE_PLAYER
+    };
+
+    const newState = playerReducer(initialState, action);
+
+    assert.equal(newState.podcast, null);
+    assert.equal(newState.currentTime, 0);
+    assert.notOk(newState.isPlaying);
+
   });
 });
 
