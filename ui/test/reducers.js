421181a91a97f0937bcbd96e7be742272211d710
Dan Jacob <danjac354@gmail.com>
2016-01-12 01:00:46 +0200

    Reducer tests

--- ui/test/reducers.js
+++ ui/test/reducers.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { Actions } from '../constants';
+import { Actions, Alerts } from '../constants';
 import channelsReducer from '../reducers/channels';
 import alertsReducer from '../reducers/alerts';
 
@@ -49,6 +49,25 @@ describe('Channels', function() {
 
     assert.equal(newState.filter, "");
 
+  });
+
+});
+
+describe("Add an alert", function() {
+
+  it("Adds a new alert", function() {
+      const state = [];
+
+      const action = {
+        type: Actions.ADD_ALERT,
+        payload: {
+          status: Alerts.SUCCESS,
+          message: "it worked!"
+        }
+      };
+
+      const newState = alertsReducer(state, action);
+      assert.equal(newState.length, 1);
   });
 
 });
