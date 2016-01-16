f77a21b696e7ee24d46c66dee90687592ffef1ec
Dan Jacob <danjac354@gmail.com>
2016-01-16 22:22:08 +0200

    More tests

--- decoders/decoders_test.go
+++ decoders/decoders_test.go
@@ -2,6 +2,86 @@ package decoders
 
 import "testing"
 
+func TestNewChannelIfURLEmpty(t *testing.T) {
+	d := &NewChannel{}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Channel should have URL")
+	}
+}
+
+func TestNewChannelIfURLInvalid(t *testing.T) {
+	d := &NewChannel{
+		URL: "testing!",
+	}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Channel should have a valid URL")
+	}
+}
+
+func TestNewChannelIfURLValid(t *testing.T) {
+	d := &NewChannel{
+		URL: "http://google.com",
+	}
+	if err := d.Decode(); err != nil {
+		t.Fatal("Channel should have a valid URL")
+	}
+}
+
+func TestNewEmailIfEmpty(t *testing.T) {
+	d := &NewEmail{}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Should be valid")
+	}
+}
+
+func TestNewEmailTrimmed(t *testing.T) {
+	d := &NewEmail{
+		Email: "    TEST@gmail.com",
+	}
+	if err := d.Decode(); err != nil {
+		t.Fatal("Should be valid")
+	}
+	if d.Email != "test@gmail.com" {
+		t.Fatal("Email should be lowercase and trimmed")
+	}
+}
+
+func TestNewPasswordIfEmpty(t *testing.T) {
+	d := &NewPassword{}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Should have error if no password fields")
+	}
+}
+
+func TestNewPasswordIfOldPasswordMissing(t *testing.T) {
+	d := &NewPassword{
+		NewPassword: "testpass",
+	}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Should have error if no password fields")
+	}
+}
+
+func TestNewPasswordIfNewPasswordTooShort(t *testing.T) {
+	d := &NewPassword{
+		OldPassword: "test",
+		NewPassword: "test",
+	}
+	if err := d.Decode(); err == nil {
+		t.Fatal("Should have error if new password too short")
+	}
+}
+
+func TestNewPasswordIfValid(t *testing.T) {
+	d := &NewPassword{
+		OldPassword: "test",
+		NewPassword: "testpass",
+	}
+	if err := d.Decode(); err != nil {
+		t.Fatal("Should have error if new password too short")
+	}
+}
+
 func TestRecoverPasswordIfNoIdentifier(t *testing.T) {
 	d := &RecoverPassword{}
 	if err := d.Decode(); err == nil {
