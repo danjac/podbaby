33b23d53c4ae7654b02605f5a043b7f52d7c81c8
Dan Jacob <danjac354@gmail.com>
2016-01-18 21:22:26 +0200

    More auth tests

--- server/auth_test.go
+++ server/auth_test.go
@@ -16,9 +16,41 @@ type mockIsEmailExistsUserReader struct {
 
 func (db *mockIsEmailExistsUserReader) IsEmail(email string, userID int64) (bool, error) {
 	if email == "test@gmail.com" {
-		return userID == 1, nil
+		return userID != 1, nil
 	}
 	return false, nil
+}
+
+func TestIsEmailIfLoggedInAndAnotherEmailExists(t *testing.T) {
+
+	user := &models.User{
+		ID: 2,
+	}
+
+	getContext = mockGetContextWithUser(user)
+
+	req, _ := http.NewRequest("GET", "/?email=test@gmail.com", nil)
+	w := httptest.NewRecorder()
+	s := &Server{
+		DB: &database.DB{
+			Users: &database.UserDB{
+				UserReader: &mockIsEmailExistsUserReader{},
+			},
+		},
+		Render: render.New(),
+	}
+
+	if err := isEmail(s, w, req); err != nil {
+		t.Fatal(err)
+	}
+
+	if w.Code != http.StatusOK {
+		t.Fatal("Should return a 200 OK")
+	}
+
+	if !strings.Contains(w.Body.String(), "true") {
+		t.Fatalf("should return false if user's own email:%s", w.Body.String())
+	}
 }
 
 func TestIsEmailIfLoggedInAndEmailExists(t *testing.T) {
@@ -49,10 +81,36 @@ func TestIsEmailIfLoggedInAndEmailExists(t *testing.T) {
 	}
 
 	if !strings.Contains(w.Body.String(), "false") {
-		t.Fatal("should return false if user's own email")
+		t.Fatalf("should return false if user's own email:%s", w.Body.String())
 	}
 }
 
+func TestIsEmailIfNotLoggedInAndEmailNotExists(t *testing.T) {
+
+	getContext = mockGetContext(make(map[string]interface{}))
+
+	req, _ := http.NewRequest("GET", "/?email=test2@gmail.com", nil)
+	w := httptest.NewRecorder()
+	s := &Server{
+		DB: &database.DB{
+			Users: &database.UserDB{
+				UserReader: &mockIsEmailExistsUserReader{},
+			},
+		},
+		Render: render.New(),
+	}
+
+	if err := isEmail(s, w, req); err != nil {
+		t.Fatal(err)
+	}
+	if w.Code != http.StatusOK {
+		t.Fatal("Should return a 200 OK")
+	}
+	if !strings.Contains(w.Body.String(), "false") {
+		t.Fatal("should return false if used email")
+	}
+
+}
 func TestIsEmailIfNotLoggedInAndEmailExists(t *testing.T) {
 
 	getContext = mockGetContext(make(map[string]interface{}))
