847c93a422efc9d5c5fe87bd20c6576d1ea564f6
Dan Jacob <danjac354@gmail.com>
2016-01-20 11:56:10 +0200

    Tidy up

--- ui/actions/utils.js
+++ ui/actions/utils.js
@@ -1,12 +1,13 @@
-import _ from 'lodash';
 import { bindActionCreators } from 'redux';
 
-export const createAction = (type, payload) => _.merge({ type }, { payload });
+export const createAction = (type, payload) => {
+  return { type, payload };
+};
 
 export const bindAllActionCreators = (actionCreators, dispatch) => {
   return Object.keys(actionCreators).reduce((result, key) => {
-      return Object.assign({}, result, {
-        [key] : bindActionCreators(actionCreators[key], dispatch)
-        });
+    return Object.assign({}, result, {
+      [key]: bindActionCreators(actionCreators[key], dispatch),
+    });
   }, {});
-}
+};
