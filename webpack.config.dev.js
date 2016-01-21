506e5060c2892667c0d15e79ed01342a2feaad02
Dan Jacob <danjac354@gmail.com>
2016-01-21 20:34:15 +0200

    Webpack config changes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,61 +4,64 @@ var HotModuleReplacementPlugin = webpack.HotModuleReplacementPlugin;
 var DefinePlugin = webpack.DefinePlugin;
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-require('es6-promise').polyfill();
 
 var entry = [
   'webpack-dev-server/client?http://localhost:8080',
   'webpack/hot/only-dev-server',
   'babel-polyfill',
-  './main.js'
+  './main.js',
 ];
 
 var plugins = [
   new DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('dev')
+    'process.env': {
+      NODE_ENV: JSON.stringify('dev'),
+    },
   }),
   new HotModuleReplacementPlugin(),
   new ExtractTextPlugin('[name].css', {
-    allChunks: true
-  })
+    allChunks: true,
+  }),
 ];
 
 
+require('es6-promise').polyfill();
+
 module.exports = {
   context: path.join(__dirname, 'ui'),
-  devtool: 'source-map',
+  devtool: 'eval',
   entry: entry,
   output: {
     path: path.join(__dirname, 'static'),
-    filename: "[name].js",
-    publicPath: 'http://localhost:8080/static/'
+    filename: '[name].js',
+    publicPath: 'http://localhost:8080/static/',
   },
   plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)/,
-        loader: 'url-loader?limit=200000'
+        loader: 'url-loader?limit=200000',
       },
       {
         test: /\.js$/,
         loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0'],
         include: path.join(__dirname, 'ui'),
-        exclude: path.join(__dirname, 'node_modules')
-      }
-    ]
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+    ],
   },
   resolve: {
     root: path.join(__dirname),
     extensions: ['', '.js'],
-  }
+  },
 
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,56 +4,61 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var DefinePlugin = webpack.DefinePlugin;
 
-require('es6-promise').polyfill();
 
 var entry = ['babel-polyfill', './main.js'];
 
 var plugins = [
   new DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('prod')
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
   }),
   new ExtractTextPlugin('[name].css', {
-    allChunks: true
+    allChunks: true,
   }),
-  new UglifyJsPlugin({ minimize: true })
+  new UglifyJsPlugin({ minimize: true }),
 ];
 
+require('es6-promise').polyfill();
+
 module.exports = {
+  cache: true,
   context: path.join(__dirname, 'ui'),
   entry: entry,
   output: {
     path: path.join(__dirname, 'static'),
-    filename: "[name].js"
+    filename: '[name].js',
   },
+  devtool: 'cheap-module-source-map',
   plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)/,
-        loader: 'url-loader?limit=200000'
+        loader: 'url-loader?limit=200000',
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-0']
+          presets: ['react', 'es2015', 'stage-0'],
         },
         include: path.join(__dirname, 'ui'),
-        exclude: path.join(__dirname, 'node_modules')
-      }
-    ]
+        exclude: path.join(__dirname, 'node_modules'),
+      },
+    ],
   },
   resolve: {
     root: path.join(__dirname),
-    extensions: ['', '.js']
-  }
+    extensions: ['', '.js'],
+  },
 
 };
