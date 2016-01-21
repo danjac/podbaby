fdd1a91d256203cb57f2181c7fe854c79f495517
Dan Jacob <danjac354@gmail.com>
2016-01-21 10:48:06 +0200

    React hot loader enabled

---
+++ react-dev-server.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config.dev');
+
+var port = 8080;
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  colors: true,
+  historyApiFallback: true,
+}).listen(port, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:' + port);
+});
