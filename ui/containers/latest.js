55d01c7fe64c569dfaad4f0ff395eb138c4b7a20
Dan Jacob <danjac354@gmail.com>
2016-01-27 02:15:29 +0200

    Refresh button on latest page

--- ui/containers/latest.js
+++ ui/containers/latest.js
@@ -1,11 +1,14 @@
 import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import DocumentTitle from 'react-document-title';
+import { Button } from 'react-bootstrap';
 
 import * as actions from '../actions';
 import { podcastsSelector } from '../selectors';
 
+import Icon from '../components/icon';
 import PodcastList from '../components/podcasts';
 import { getTitle } from './utils';
 
@@ -13,14 +16,20 @@ export class Latest extends React.Component {
 
   constructor(props) {
     super(props);
+    this.actions = bindActionCreators(actions.latest, this.props.dispatch);
     this.handleSelectPage = this.handleSelectPage.bind(this);
+    this.handleRefresh = this.handleRefresh.bind(this);
   }
 
   handleSelectPage(event, selectedEvent) {
     event.preventDefault();
-    const { dispatch } = this.props;
     const page = selectedEvent.eventKey;
-    dispatch(actions.latest.getLatestPodcasts(page));
+    this.actions.getLatestPodcasts(page);
+  }
+
+  handleRefresh(event) {
+    event.prevetDefault();
+    this.actions.getLatestPodcasts(1);
   }
 
   render() {
@@ -30,12 +39,20 @@ export class Latest extends React.Component {
 
     return (
       <DocumentTitle title={getTitle('Latest podcasts')}>
+        <div>
+        {this.props.page.page === 1 ?
+        <form className="form">
+          <Button bsStyle="primary" className="form-control">
+            <Icon icon="refresh" /> Update
+          </Button>
+        </form> : ''}
         <PodcastList
           actions={actions}
           ifEmpty={ifEmptyMsg}
           onSelectPage={this.handleSelectPage}
           showChannel {...this.props}
         />
+        </div>
       </DocumentTitle>
     );
   }
